6d37a5fa3ee32925284f0f2d98504349985c8d89
TinkaKarolinka <smolarekkarolina1999@gmail.com>
2022-12-01 19:24:56 +0100

    Add new project

---
+++ src/ChallengeApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChallengeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee("Karolina");
+            employee.AddGrade(34.4);
+
+
+            var stat = employee.GetStatistics();
+
+            Console.WriteLine($"The average is: {stat.Average}");
+
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Hello " + args[0]);
+            }
+            else
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+
+    }
+}
+
---
+++ test/Challenge.Tests/EmployeeTests.cs
@@ -0,0 +1,24 @@
+using System;
+using ChallengeApp;
+using Xunit;
+
+//namespace Challenge.Tests
+{
+  //  public class EmployeeTests
+    {
+      //  [Fact]
+      //  public void Test1()
+        {
+           // var emp = new Employee("Karolina");
+          //  emp.AddGrade(34.4);
+          //  emp.AddGrade(33.1);
+         //   emp.AddGrade(80.5);
+
+          //  var result = emp.GetStatistics();
+
+           // Assert.Equal(49.3, result.Average, 1);
+          //  Assert.Equal(80.5, result.High);
+           // Assert.Equal(33.1, result.Low);
+        }
+    }
+}
